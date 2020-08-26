f28eac4dbf2eccc3a2dbb32f2dfeecc45f98d927
brentthayer1 <bthayer1@gmail.com>
2020-08-25 18:03:19 -0600

    end day 2

--- src/funcs.py
+++ src/funcs.py
@@ -1,148 +1,253 @@
-from __future__ import division
-import numpy as np
-from numpy.random import choice
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-import os
-from collections import Counter
+import tensorflow as tf
+from tensorflow.keras.callbacks import TensorBoard
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.utils import np_utils
-from keras.optimizers import SGD
-import cv2
-import random
-from tensorflow import keras
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras.utils import to_categorical
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense, BatchNormalization
+from keras import backend as K
+from keras import metrics
+import datetime
 
-import numpy as np
-np.random.seed(42)
+# DIRECTORIES
+train_data_dir = '/Volumes/b/Galvanize/DS-RFT4/capstones-RFT4/datasets/covid_pneumonia/pneum_normal/train'
+test_data_dir = '/Volumes/b/Galvanize/DS-RFT4/capstones-RFT4/datasets/covid_pneumonia/pneum_normal/test'
 
-def load_data(DATADIR, FOLDER, categories, img_rows, img_cols):
-    data = []
-    for category in categories.keys():
-        path = DATADIR + FOLDER + category
-        files = os.listdir(path)
-        category_num = categories[category]
+nb_classes = 1
+class_weights = {0 : 1,
+                1 : 1}
+seed = 42
 
-        for im in files:
-            img_array = cv2.imread(os.path.join(path, im), cv2.IMREAD_GRAYSCALE)
-            new_array = cv2.resize(img_array, (img_rows, img_cols))
-            data.append((new_array, category_num))
+validation_split = 0.2
+img_rows, img_cols = 255, 255
 
-    random.shuffle(data)
+batch_size = 32
+nb_epoch = 3     
 
-    X = []
-    y = []
+# CONV2D 1
+nb_filters_1 = 32
+kernel_size_1 = (3, 3)
+stride_size_1 = (1, 1)
+pool_size_1 = (2, 2)
+activation_1 = 'swish'
+dropout_1 = 0.7
 
-    for features, label in data:
-        X.append(np.array(features))
-        y.append(label)
+# CONV2D 2
+nb_filters_2 = 64
+kernel_size_2 = (3, 3)
+stride_size_2 = (1, 1)
+pool_size_2 = (2, 2)
+activation_2 = 'swish'
+dropout_2 = 0.8
 
-    X = np.array(X).reshape(-1, img_rows, img_cols)
-    # X = X / 255
-    y = np.array(y)
-    
-    return X, y
+# CONV2D 3
+nb_filters_3 =  128
+kernel_size_3 = (3, 3)
+stride_size_3 = (2, 2)
+pool_size_3 = (2, 2)
+activation_3 = 'swish'
+dropout_3 = 0.7
 
-def load_and_featurize_data(X, y, img_rows, img_cols):
+# CONV2D 4
+nb_filters_4 = 256
+kernel_size_4 = (3, 3)
+stride_size_4 = (2, 2)
+pool_size_4 = (2, 2)
+activation_4 = 'swish'
+dropout_4 = 0.7
 
-    X_train, X_test, Y_train, Y_test = train_test_split(X, y)
+# CONV2D 5
+nb_filters_5 = 512
+kernel_size_5 = (3, 3)
+stride_size_5 = (2, 2)
+pool_size_5 = (2, 2)
+activation_5 = 'swish'
+dropout_5 = 0.6
 
-    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols)
-    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols)
+# DENSE 6
+units_6 = 512
+activation_6 = 'swish'
+dropout_6 = 0.7
 
-    X_train = X_train.astype('float32')
-    X_test = X_test.astype('float32')
-    X_train /= 255
-    X_test /= 255
+# DENSE 7
+units_7 = 256
+activation_7 = 'swish'
+dropout_7 = 0.6
 
-    print('X_train shape:', X_train.shape)
-    print(X_train.shape[0], 'train samples')
-    print(X_test.shape[0], 'test samples')
+# DENSE 8
+units_8 = 128
+activation_8 = 'swish'
+dropout_8 = 0.7
 
-    # convert class vectors to binary class matrices (don't change)
-    # Y_train = to_categorical(y_train, nb_classes)  # cool
-    # Y_test = to_categorical(y_test, nb_classes)
-    return X_train, X_test, Y_train, Y_test
+# DENSE 9
+units_9 = 64
+activation_9 = 'swish'
+dropout_9 = 0.8
 
+# DENSE 10
+units_10 = 32
+activation_10 = 'swish'
+dropout_10 = 0.7
 
-def define_model(nb_filters, kernel_size, input_shape, pool_size):
-    model = Sequential()  # model is a linear stack of layers (don't change)
+# DENSE 11
+units_11 = 16
+activation_11 = 'swish'
+dropout_11 = 0.6
 
-    # note: the convolutional layers and dense layers require an activation function
-    # see https://keras.io/activations/
-    # and https://en.wikipedia.org/wiki/Activation_function
-    # options: 'linear', 'sigmoid', 'tanh', 'relu', 'softplus', 'softsign'
+# DENSE 12
+units_12 = 8
+activation_12 = 'swish'
+dropout_12 = 0.5
 
-    model.add(Conv2D(nb_filters,
-                     (kernel_size[0], kernel_size[1]),
-                     padding='valid',
-                     input_shape=input_shape))  # first conv. layer  KEEP
-    model.add(Activation('tanh'))  # Activation specification necessary for Conv2D and Dense layers
-
-    model.add(Conv2D(nb_filters,
-                     (kernel_size[0], kernel_size[1]),
-                     padding='valid'))  # 2nd conv. layer KEEP
-    model.add(Activation('relu'))
-
-    model.add(MaxPooling2D(pool_size=pool_size))  # decreases size, helps prevent overfitting
-    model.add(Dropout(0.5))  # zeros out some fraction of inputs, helps prevent overfitting
-
-    model.add(Flatten())  # necessary to flatten before going into conventional dense layer  KEEP
-    print('Model flattened out to ', model.output_shape)
-
-    # now start a typical neural network
-    model.add(Dense(32))  # (only) 32 neurons in this layer, really?   KEEP
-    model.add(Activation('relu'))
-
-    model.add(Dropout(0.5))  # zeros out some fraction of inputs, helps prevent overfitting
-
-    model.add(Dense(3))  # 10 final nodes (one for each class)  KEEP
-    model.add(Activation('softmax'))  # softmax at end to pick between classes 0-9 KEEP
-
-    # many optimizers available, see https://keras.io/optimizers/#usage-of-optimizers
-    # suggest you KEEP loss at 'categorical_crossentropy' for this multiclass problem,
-    # and KEEP metrics at 'accuracy'
-    # suggest limiting optimizers to one of these: 'adam', 'adadelta', 'sgd'
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-    return model
-
-if __name__ == '__main__':
-
-    DATADIR = '/Volumes/b/Galvanize/DS-RFT4/capstones-RFT4/datasets/covid_pneumonia/small_data_set'
-    FOLDER = '/train/'
-    categories = {'COVID-19/' : 0, 'normal/' : 1, 'pneumonia/' : 2} 
-
-    batch_size = 15  # number of training samples used at a time to update the weights
-    # nb_classes = 10    # number of output possibilities: [0 - 9] KEEP
-    nb_epoch = 2       # number of passes through the entire train dataset before weights "final"
-    img_rows, img_cols = 255, 255   # the size of the MNIST images KEEP
-    input_shape = (img_rows, img_cols, 1)   # 1 channel image input (grayscale) KEEP
-    nb_filters = 12    # number of convolutional filters to use
-    pool_size = (2, 2)  # pooling decreases image size, reduces computation, adds translational invariance
-    kernel_size = (4, 4)  # convolutional kernel size, slides over image to learn features
-
-    X, y = load_data(DATADIR, FOLDER, categories, img_rows, img_cols)
-    X_train, X_test, Y_train, Y_test = load_and_featurize_data(X, y, img_rows, img_cols)
+# COMPILE
+compile_optimizer = 'adam'
 
 
-    model = define_model(nb_filters, kernel_size, input_shape, pool_size)
+# CNN
+if K.image_data_format() == 'channels_first':
+    input_shape = (1, img_cols, img_rows)
+    chanDim = 1
+else:
+    input_shape = (img_cols, img_rows, 1)
+    chanDim = -1
 
-    # during fit process watch train and test error simultaneously
-    model.fit(X_train, Y_train, batch_size=batch_size, epochs=nb_epoch,
-              verbose=1, validation_data=(X_test, Y_test))
+model = Sequential()
 
-    score = model.evaluate(X_test, Y_test, verbose=0)
-    print('Test score:', score[0])
-    print('Test accuracy:', score[1])  # this is the one we care about
+model.add(Conv2D(nb_filters_1,
+                    (kernel_size_1[0], kernel_size_1[1]),
+                    strides=(stride_size_1[0], stride_size_1[1]),
+                    padding='same', activation=activation_1,
+                    input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=pool_size_1))
+# model.add(BatchNormalization(axis=chanDim))
+model.add(Dropout(dropout_1)) 
 
+model.add(Conv2D(nb_filters_2,
+                    (kernel_size_2[0], kernel_size_2[1]),
+                    strides=(stride_size_2[0], stride_size_2[1]),
+                    padding='same', activation=activation_2))
+model.add(MaxPooling2D(pool_size=pool_size_2))
+model.add(BatchNormalization(axis=chanDim))
+model.add(Dropout(dropout_2)) 
 
+# model.add(Conv2D(nb_filters_3,
+#                     (kernel_size_3[0], kernel_size_3[1]),
+#                     strides=(stride_size_3[0], stride_size_3[1]),
+#                     padding='same', activation=activation_3))
+# model.add(MaxPooling2D(pool_size=pool_size_3))
+# model.add(BatchNormalization(axis=chanDim))
+# model.add(Dropout(dropout_3)) 
 
+# model.add(Conv2D(nb_filters_4,
+#                     (kernel_size_4[0], kernel_size_4[1]),
+#                     strides=(stride_size_4[0], stride_size_4[1]),
+#                     padding='same', activation=activation_4))
+# model.add(MaxPooling2D(pool_size=pool_size_4))
+# model.add(BatchNormalization(axis=chanDim))
+# model.add(Dropout(dropout_4))
 
+# model.add(Conv2D(nb_filters_5,
+#                     (kernel_size_5[0], kernel_size_5[1]),
+#                     strides=(stride_size_5[0], stride_size_5[1]),
+#                     padding='same', activation=activation_5))
+# model.add(MaxPooling2D(pool_size=pool_size_5))
+# model.add(BatchNormalization(axis=chanDim))
+# model.add(Dropout(dropout_5))
 
+model.add(Flatten())
+
+# model.add(Dense(units_6, activation=activation_6))
+# model.add(Dropout(dropout_6))
+
+# model.add(Dense(units_7, activation=activation_7))
+# model.add(Dropout(dropout_7))
+
+# model.add(Dense(units_8, activation=activation_8))
+# model.add(Dropout(dropout_8))
+
+model.add(Dense(units_9, activation=activation_9))
+model.add(Dropout(dropout_9))
+
+model.add(Dense(units_10, activation=activation_10))
+model.add(Dropout(dropout_10))
+
+model.add(Dense(units_11, activation=activation_11))
+model.add(Dropout(dropout_11))
+
+model.add(Dense(units_12, activation=activation_12))
+model.add(Dropout(dropout_12))
+
+model.add(Dense(nb_classes, activation='sigmoid'))
+
+METRICS = [
+            metrics.BinaryAccuracy(name='ACCURACY'),
+            metrics.Precision(name='PRECISION'),
+            metrics.Recall(name='RECALL'),
+            metrics.AUC(name='AUC'),
+            metrics.TruePositives(name='TP'),
+            metrics.TrueNegatives(name='TN'),
+            metrics.FalsePositives(name='FP'),
+            metrics.FalseNegatives(name='FN')]
+
+model.compile(loss='binary_crossentropy',
+                optimizer=compile_optimizer,
+                metrics=METRICS)
+
+# GENERATORS
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    validation_split=validation_split)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_cols, img_rows),
+    color_mode='grayscale',
+    batch_size=batch_size,
+    class_mode='binary',
+    shuffle=True,
+    subset='training',
+    seed=seed)
+
+validation_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_cols, img_rows),
+    color_mode='grayscale',
+    batch_size=batch_size,
+    class_mode='binary',
+    shuffle=True,
+    subset='validation',
+    seed=seed)
+
+test_generator = test_datagen.flow_from_directory(
+    test_data_dir,
+    target_size=(img_cols, img_rows),
+    color_mode='grayscale',
+    batch_size=batch_size,
+    class_mode='binary',
+    seed=seed)
+
+# TENSORBOARD
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+# FIT
+model.fit(
+    train_generator,
+    steps_per_epoch= train_generator.samples // batch_size,
+    epochs=nb_epoch,
+    validation_data= validation_generator,
+    validation_steps= validation_generator.samples // batch_size
+    # class_weight=class_weights
+    # callbacks=[tensorboard_callback]
+    )
+
+# model.save_weights('first_try.h5')
+
+model.summary()
+
+# label_map = (train_generator.class_indices)
+# print(label_map)
