823b11afab531bce58a6b0046b8c75d930193658
brentthayer1 <bthayer1@gmail.com>
2020-08-26 18:05:05 -0600

    no scaling

---
+++ src/cnn.py
@@ -0,0 +1,373 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+
+auth.authenticate_user()
+gauth = GoogleAuth()
+gauth.credentials = GoogleCredentials.get_application_default()
+drive = GoogleDrive(gauth)
+
+downloaded = drive.CreateFile({'id':"1_gaHq3ubfIokmlLX7a47C7Sq01vq4utp"})
+downloaded.GetContentFile('pneumonia.zip')
+
+# downloaded = drive.CreateFile({'id':"1p5dyUcpCWGHwA_O5o6nUrL25UhkYqNGK"})
+# downloaded.GetContentFile('pneum_normal_copy.zip')
+
+
+!unzip pneumonia.zip
+
+# !unzip pneum_normal_copy.zip
+
+
+import tensorflow as tf
+from tensorflow.keras.callbacks import TensorBoard
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense, BatchNormalization
+from keras import backend as K
+from keras import metrics
+import datetime
+from time import time
+from tensorboardcolab import TensorBoardColab, TensorBoardColabCallback
+from keras.optimizers import Adam, RMSprop
+
+
+# train_data_dir = '/content/pneum_normal_copy/train'
+# test_data_dir = '/content/pneum_normal_copy/test'
+
+train_data_dir = '/content/chest_xray/train'
+test_data_dir = '/content/chest_xray/val'
+val_data_dir = '/content/chest_xray/test'
+
+
+nb_classes = 1
+class_weights = {0 : 1,
+                1 : 1}
+seed = 42
+
+# validation_split = 0.2
+img_rows, img_cols = 300, 300
+
+batch_size = 16
+nb_epoch = 10     
+
+# CONV2D 1
+nb_filters_1 = 16
+kernel_size_1 = (3, 3)
+stride_size_1 = (1, 1)
+pool_size_1 = (2, 2)
+activation_1 = 'relu'
+dropout_1 = 0.8
+
+# CONV2D 2
+nb_filters_2 = 32
+kernel_size_2 = (3, 3)
+stride_size_2 = (1, 1)
+pool_size_2 = (2, 2)
+activation_2 = 'relu'
+dropout_2 = 0.8
+
+# CONV2D 3
+nb_filters_3 =  64
+kernel_size_3 = (3, 3)
+stride_size_3 = (2, 2)
+pool_size_3 = (2, 2)
+activation_3 = 'relu'
+dropout_3 = 0.8
+
+# CONV2D 4
+nb_filters_4 = 128
+kernel_size_4 = (3, 3)
+stride_size_4 = (2, 2)
+pool_size_4 = (2, 2)
+activation_4 = 'relu'
+dropout_4 = 0.7
+
+# CONV2D 5
+nb_filters_5 = 256
+kernel_size_5 = (3, 3)
+stride_size_5 = (2, 2)
+pool_size_5 = (2, 2)
+activation_5 = 'relu'
+# dropout_5 = 0.8
+
+# DENSE 6
+units_6 = 512
+activation_6 = 'relu'
+# dropout_6 = 0.8
+
+# # DENSE 7
+# units_7 = 256
+# activation_7 = 'relu'
+# dropout_7 = 0.8
+
+# # DENSE 8
+# units_8 = 128
+# activation_8 = 'relu'
+# dropout_8 = 0.8
+
+# # DENSE 9
+# units_9 = 64
+# activation_9 = 'swish'
+# dropout_9 = 0.8
+
+# # DENSE 10
+# units_10 = 32
+# activation_10 = 'swish'
+# dropout_10 = 0.7
+
+# # DENSE 11
+# units_11 = 16
+# activation_11 = 'swish'
+# dropout_11 = 0.6
+
+# # DENSE 12
+# units_12 = 8
+# activation_12 = 'swish'
+# dropout_12 = 0.5
+
+# COMPILE
+compile_optimizer = RMSprop(lr=0.001)
+
+
+# CNN
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_cols, img_rows)
+    chanDim = 1
+else:
+    input_shape = (img_cols, img_rows, 3)
+    chanDim = -1
+
+model = Sequential()
+
+model.add(Conv2D(nb_filters_1,
+                    (kernel_size_1[0], kernel_size_1[1]),
+                    strides=(stride_size_1[0], stride_size_1[1]),
+                    padding='valid', activation=activation_1,
+                    input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=pool_size_1))
+# model.add(BatchNormalization(axis=chanDim))
+# model.add(Dropout(dropout_1)) 
+
+model.add(Conv2D(nb_filters_2,
+                    (kernel_size_2[0], kernel_size_2[1]),
+                    strides=(stride_size_2[0], stride_size_2[1]),
+                    padding='valid', activation=activation_2))
+model.add(MaxPooling2D(pool_size=pool_size_2))
+# model.add(BatchNormalization(axis=chanDim))
+# model.add(Dropout(dropout_2)) 
+
+model.add(Conv2D(nb_filters_3,
+                    (kernel_size_3[0], kernel_size_3[1]),
+                    strides=(stride_size_3[0], stride_size_3[1]),
+                    padding='valid', activation=activation_3))
+model.add(MaxPooling2D(pool_size=pool_size_3))
+# model.add(BatchNormalization(axis=chanDim))
+# model.add(Dropout(dropout_3)) 
+
+model.add(Conv2D(nb_filters_4,
+                    (kernel_size_4[0], kernel_size_4[1]),
+                    strides=(stride_size_4[0], stride_size_4[1]),
+                    padding='valid', activation=activation_4))
+model.add(MaxPooling2D(pool_size=pool_size_4))
+# model.add(BatchNormalization(axis=chanDim))
+# model.add(Dropout(dropout_4))
+
+model.add(Conv2D(nb_filters_5,
+                    (kernel_size_5[0], kernel_size_5[1]),
+                    strides=(stride_size_5[0], stride_size_5[1]),
+                    padding='same', activation=activation_5))
+model.add(MaxPooling2D(pool_size=pool_size_5))
+# model.add(BatchNormalization(axis=chanDim))
+# model.add(Dropout(dropout_5))
+
+model.add(Flatten())
+
+model.add(Dense(units_6, activation=activation_6))
+# model.add(Dropout(dropout_6))
+
+# model.add(Dense(units_7, activation=activation_7))
+# model.add(Dropout(dropout_7))
+
+# model.add(Dense(units_8, activation=activation_8))
+# model.add(Dropout(dropout_8))
+
+# model.add(Dense(units_9, activation=activation_9))
+# model.add(Dropout(dropout_9))
+
+# model.add(Dense(units_10, activation=activation_10))
+# model.add(Dropout(dropout_10))
+
+# model.add(Dense(units_11, activation=activation_11))
+# model.add(Dropout(dropout_11))
+
+# model.add(Dense(units_12, activation=activation_12))
+# model.add(Dropout(dropout_12))
+
+model.add(Dense(nb_classes, activation='sigmoid'))
+
+METRICS = [
+            metrics.BinaryAccuracy(name='ACCURACY'),
+            metrics.Precision(name='PRECISION'),
+            metrics.Recall(name='RECALL'),
+            metrics.AUC(name='AUC'),
+            metrics.TruePositives(name='TP'),
+            metrics.TrueNegatives(name='TN'),
+            metrics.FalsePositives(name='FP'),
+            metrics.FalseNegatives(name='FN')]
+
+model.compile(loss='binary_crossentropy',
+                optimizer=compile_optimizer,
+                metrics=METRICS
+              )
+
+# GENERATORS
+train_datagen = ImageDataGenerator(
+    # rescale=1. / 255,
+    # shear_range=0.2,
+    # zoom_range=0.2,
+    # horizontal_flip=True
+    # validation_split=validation_split
+    )
+
+val_datagen = ImageDataGenerator(
+    # rescale=1. / 255,
+    # shear_range=0.2,
+    # zoom_range=0.2,
+    # horizontal_flip=True
+    )
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_cols, img_rows),
+    # color_mode='grayscale',
+    batch_size=batch_size,
+    class_mode='binary',
+    shuffle=True
+    # subset='training',
+    # seed=seed
+    )
+
+validation_generator = val_datagen.flow_from_directory(
+    val_data_dir,
+    target_size=(img_cols, img_rows),
+    # color_mode='grayscale',
+    batch_size=batch_size,
+    class_mode='binary',
+    shuffle=True
+    # subset='validation',
+    # seed=seed
+    )
+
+test_generator = test_datagen.flow_from_directory(
+    test_data_dir,
+    target_size=(img_cols, img_rows),
+    # color_mode='grayscale',
+    batch_size=batch_size,
+    shuffle=False,
+    class_mode='binary'
+    # seed=seed
+    )
+
+# FIT
+history = model.fit(
+    train_generator,
+    steps_per_epoch= train_generator.samples // batch_size,
+    epochs=nb_epoch,
+    validation_data= validation_generator,
+    validation_steps= validation_generator.samples // batch_size
+    # class_weight=class_weights
+    # callbacks=[tensorboard_callback]
+    )
+
+
+
+model.summary()
+
+label_map = (train_generator.class_indices)
+print(label_map)
+
+
+from sklearn.metrics import classification_report, roc_curve, auc, confusion_matrix
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+
+
+
+# make a prediction
+y_pred = model.predict_generator(test_generator, test_generator.samples // test_generator.batch_size+1) #(test_gen, steps=len(df_val), verbose=1)
+fpr, tpr, thresholdss = roc_curve(test_generator.classes, y_pred)
+auc_ = auc(fpr, tpr)
+
+
+fig, ax = plt.subplots(1,2, figsize=(10, 5))
+
+#roc
+ax[0].plot([0, 1], [0, 1], 'k--')
+ax[0].plot(fpr, tpr, label='area = {:.3f}'.format(auc_))
+ax[0].set_xlabel('False Positive Rate')
+ax[0].set_ylabel('True Positive Rate')
+ax[0].set_title('ROC')
+ax[0].legend(loc='best')
+
+# #auc
+ax[1].plot(history.history['AUC'])
+ax[1].plot(history.history['val_AUC'])
+ax[1].set_title('AUC')
+ax[1].set_ylabel('AUC')
+ax[1].set_xlabel('Epoch')
+ax[1].legend(['train', 'val'], loc='best')
+plt.tight_layout()
+
+
+
+Y_pred = model.predict_generator(test_generator, test_generator.samples // batch_size + 1)
+y_pred = np.argmax(Y_pred, axis=1)
+print('Confusion Matrix')
+print(confusion_matrix(test_generator.classes, y_pred))
+print('Classification Report')
+target_names = test_generator.classes
+class_labels = list(test_generator.class_indices.keys())   
+report = classification_report(target_names, y_pred, target_names=class_labels)
+print()
+print(report) 
+
+fig, ax = plt.subplots(2, 2, figsize=(12,6))
+#accuracy
+ax[0][0].plot(history.history['ACCURACY'])
+ax[0][0].plot(history.history['val_ACCURACY'])
+ax[0][0].set_title('Model Accuracy')
+ax[0][0].set_ylabel('Accuracy')
+ax[0][0].set_xlabel('Epoch')
+ax[0][0].legend(['train', 'test'], loc='best')
+
+#loss
+ax[0][1].plot(history.history['loss'])
+ax[0][1].plot(history.history['val_loss'])
+ax[0][1].set_title('Model Loss')
+ax[0][1].set_ylabel('Loss')
+ax[0][1].set_xlabel('Epoch')
+ax[0][1].legend(['train', 'test'], loc='best')
+
+#precision
+ax[1][0].plot(history.history['PRECISION'])
+ax[1][0].plot(history.history['val_PRECISION'])
+ax[1][0].set_title('Model Precision')
+ax[1][0].set_ylabel('Precision')
+ax[1][0].set_xlabel('Epoch')
+ax[1][0].legend(['train', 'test'], loc='best')
+
+#recall
+ax[1][1].plot(history.history['RECALL'])
+ax[1][1].plot(history.history['val_RECALL'])
+ax[1][1].set_title('Model Recall')
+ax[1][1].set_ylabel('Recall')
+ax[1][1].set_xlabel('Epoch')
+ax[1][1].legend(['train', 'test'], loc='best')
+plt.tight_layout()
+;
